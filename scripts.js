9752d1fa458ed35ee5eec0624070e0bdfeb87df7
Nate Shuster <shusternathaniel@gmail.com>
2022-09-15 14:30:04 -0400

    Build update grid function to allow pixel change

--- scripts.js
+++ scripts.js
@@ -21,4 +21,31 @@ function changeColor(e) {
   box.style.backgroundColor = 'purple';
 }
 
+function updateGrid(e) {
+  let pixels = e.target.value;
+  let container = document.querySelector('.grid-container');
+  container.textContent = '';
+  
+  while (container.firstChild) {
+    container.removeChild('.flex-container');
+  }
+  
+  for (let i = 0; i < pixels; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'flex-container');
+    
+    for (let i = 0; i < pixels; i++) {
+      const col = document.createElement('div');
+      col.setAttribute('class', 'box');
+      col.addEventListener('mouseover', changeColor);
+      div.appendChild(col);
+    }
+
+    container.appendChild(div);
+  }
+}
+
 createGrid();
+
+const slider = document.querySelector('.slider');
+slider.addEventListener('change', updateGrid)
